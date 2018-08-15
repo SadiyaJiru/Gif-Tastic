a0bc367497b8b319c7a33d64c20ddb31196c63b6
sadiyaj <sadiyajiru@gmail.com>
2018-08-14 21:23:45 -0500

    debugging

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,89 @@
+$( document ).ready(function() {
+  // An array of topics, new topics will be pushed into this array;
+  var topics = ["Salsa" , "Michael Jackson" , "Ball Room Dancing" , "Cha CHa" , "Milly Rock" , "Ballet" ,
+  "Macarena" , "Hip Hop" , "Bachata" , "Zumba"];
+  // Creating Functions & Methods
+  // Function that displays all gif buttons
 
-//button for all topicss
-$(".btn").on("click", function() {
-// Grabbing and storing the data-topics property value from the button
-var topics = $(this).attr("data-topics");
+// Creates a new button
+function addNewButton(){
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-topics + "&api_key=3EXHyNptM9m6Q9yAhAzpjibJWHq9P8Oq&limit=2";
-
-//For our search search
-var searchApi = "http://api.giphy.com/v1/gifs/search?" 
-+ topics + "&api_key=3EXHyNptM9m6Q9yAhAzpjibJWHq9P8Oq";
-var query = "&q=dancing";
-
-//var searchTerm = $('#searchTerm').val();
-
-console.log(searchTerm);
-
-
-// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-// url += '?' + $.param({
-//   'api-key': "3EXHyNptM9m6Q9yAhAzpjibJWHq9P8Oq&limit",
-//   'q': searchTerm,
-// });
-
-
-// Perfoming an AJAX GET request to my queryURL
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})// After the data comes back from the API
-.then(function(response) {
-  // Storing an array of results in the results variable
-  var results = response.data;
-
-  // Looping over every result item
-  for (var i = 0; i < results.length; i++) {
-
-    // Only taking action if the photo has an appropriate rating
-    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-      // Creating a div with the class "item"
-      var gifDiv = $("<div class='item'>");
-
-      // Storing the result item's rating
-      var rating = results[i].rating;
-
-      // Creating a paragraph tag with the result item's rating
-      var p = $("<p>").text("Rating: " + rating);
-
-      // Creating an image tag
-      var topicsImage = $("<img>");
-
-      // Giving the image tag an src attribute of a proprty pulled off the
-      // result item
-      topicsImage.attr("src", results[i].images.fixed_height.url);
-
-      // Appending the paragraph and topicsImage we created to the "gifDiv" div we created
-      gifDiv.append(p);
-      gifDiv.append(topicsImage);
-
-      // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-      $("#gifResults").prepend(gifDiv);
-    }
+  $("#addGif").on("click", function(){
+  var action = $("#user-input").val().trim();
+  if (action == ""){
+    return false; // added so user cannot add a blank button
   }
-});
-});
+  topics.push(action);
+  displayBtns();
+  return false;
+  });
+}
+//displays all of the buttons by appending the new ones
+function displayBtns(){
+  $("#gifButtonsView").empty(); // prevents duplication when a new button is added
+    // $("#gifButtonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the gifResults
+    for (var i = 0; i < topics.length; i++){ //loop through the array
+        var gifButton = $("<button>"); //create a new gif button in html
+        gifButton.addClass("action"); //create a class called ation
+        gifButton.addClass("btn btn-info") //
+        gifButton.attr("data-name", topics[i]); 
+        gifButton.text(topics[i]);
+        $("#gifButtonsView").append(gifButton); 
+    }
+}
+
+  // Function that displays all of the gifs
+  function displayGifs(){
+      var action = $(this).attr("data-name");
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=3EXHyNptM9m6Q9yAhAzpjibJWHq9P8Oq&limit=5";
+      console.log(queryURL); // displays the constructed url
+      $.ajax({
+          url: queryURL,
+          method: 'GET'
+      })
+      .done(function(response) {
+          console.log(response); // console test to make sure something returns
+          $("#gifsView").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
+          var gifResults = response.data; //shows results of gifs
+          if (gifResults == ""){
+            alert("There isn't a gif for this selected button");
+          }
+          for (var i=0; i<gifResults.length; i++){
+  
+              var gifDiv = $("<div>"); //div for the gifs to go inside
+              gifDiv.addClass("gifDiv");
+              // pulling rating of gif
+              var gifRating = $("<p>").text("Rating: " + gifResults[i].rating);
+              gifDiv.append(gifRating);
+              // pulling still image of gif
+
+              // pulling gif from the api
+              var gifImg = $("<img>");
+              gifImg.attr("src", gifResults[i].images.fixed_height_small_still.url); // still image stored into src of image
+              gifImg.attr("data-still",gifResults[i].images.fixed_height_small_still.url); // still image
+              gifImg.attr("data-animate",gifResults[i].images.fixed_height_small.url); // animated image
+              gifImg.attr("data-state", "still"); // set the image state
+              gifImg.addClass("image");
+              gifDiv.append(gifImg);
+              // add the giffs as the first child
+              $("#gifsView").prepend(gifDiv);
+          }
+      });
+  }
+  // Calling Functions & Methods
+  displayBtns(); // displays list of topics already created
+  addNewButton();
+  // Document Event Listeners
+  $(document).on("click", ".action", displayGifs);
+  $(document).on("click", ".image", function(){
+      var state = $(this).attr('data-state');
+      if ( state == 'still'){
+          $(this).attr('src', $(this).data('animate'));
+          $(this).attr('data-state', 'animate');
+      }else{
+          $(this).attr('src', $(this).data('still'));
+          $(this).attr('data-state', 'still');
+      }
+  });
+  });
+  
